30b089bd3fd6a75a8ab3bcd44451cc89480d9ccb
erech <sofiahanapin@gmail.com>
2023-03-09 09:39:29 -0800

    add files + install modules

---
+++ server.js
@@ -0,0 +1,23 @@
+//require express + connection + routes
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+(async () => {
+    try {
+      await db.once("open");
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+      });
+    } catch (error) {
+      console.error(`Error starting server: ${error.message}`);
+      process.exit(1);
+    }
+  })();
